d912b26369f422e50f1c5bb103fd350dbf5f5412
Ignacio Chavez <nashio@gmail.com>
2017-04-14 01:59:20 -0700

    indenting

--- components/Content.js
+++ components/Content.js
@@ -1,67 +1,67 @@
 import React from 'react';
 import '../assets/styles/index.scss';
 import {
-	navigateTo,
-	getCurrentRequestContext
+  navigateTo,
+  getCurrentRequestContext
 } from 'react-server';
 
 const DELAY = 1000;
 
 class Content extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {show: false};
-		this.pageName = props.pageName;
-		this.handleClick = this.handleClick.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {show: false};
+    this.pageName = props.pageName;
+    this.handleClick = this.handleClick.bind(this);
 
-	}
+  }
 
-	simulateAjaxCall(callback) {
-		setTimeout(callback, DELAY);
-	}
+  simulateAjaxCall(callback) {
+    setTimeout(callback, DELAY);
+  }
 
-	handleClick() {
-		this.setState({
-			show: false
-		});
+  handleClick() {
+    this.setState({
+      show: false
+    });
 
-		this.simulateAjaxCall(() => {
-			if (this.state.currentRoute == 'home') {
-				navigateTo('/about', {reuseDom: true});
-			} else {
-				navigateTo('/', {reuseDom: true});
-			}
-		});
-	}
+    this.simulateAjaxCall(() => {
+      if (this.state.currentRoute == 'home') {
+        navigateTo('/about', {reuseDom: true});
+      } else {
+        navigateTo('/', {reuseDom: true});
+      }
+    });
+  }
 
-	componentDidMount() {
-		this.setState({
-			show: true
-		});
+  componentDidMount() {
+    this.setState({
+      show: true
+    });
 
-		getCurrentRequestContext().navigator.on('loadComplete', () => {
-			// able to setState because reuseDom: true
-			this.setState({
-				show: true,
-				currentRoute: getCurrentRequestContext().navigator._currentRoute.name.toLowerCase()
-			});
-		});
-	}
+    getCurrentRequestContext().navigator.on('loadComplete', () => {
+      // able to setState because reuseDom: true
+      this.setState({
+        show: true,
+        currentRoute: getCurrentRequestContext().navigator._currentRoute.name.toLowerCase()
+      });
+    });
+  }
 
-	render() {
-		const imgUrl = this.props.img;
-		const imgClass = this.state.show && 'show';
-		const pageName = this.state.currentRoute && this.state.currentRoute;
-		const otherPage = pageName === 'home' ? 'about' : 'home';
-		return (
-			<div>
-				<h3>{pageName} Page</h3>
-				<button onClick={this.handleClick}>Navigate to {otherPage}</button>
-				<h3></h3>
-				<img src={imgUrl} alt={pageName} width="500" className={imgClass} />
-			</div>
-		)
-	}
+  render() {
+    const imgUrl = this.props.img;
+    const imgClass = this.state.show && 'show';
+    const pageName = this.state.currentRoute && this.state.currentRoute;
+    const otherPage = pageName === 'home' ? 'about' : 'home';
+    return (
+      <div>
+        <h3>{pageName} Page</h3>
+        <button onClick={this.handleClick}>Navigate to {otherPage}</button>
+        <h3></h3>
+        <img src={imgUrl} alt={pageName} width="500" className={imgClass} />
+      </div>
+    )
+  }
 };
 
 export default Content;
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import Content from '../components/Content';
 import {
-	RootElement,
-	RootContainer,
-	TheFold,
+  RootElement,
+  RootContainer,
+  TheFold,
 } from 'react-server';
 const HOME_IMG = "https://images.unsplash.com/photo-1484148793317-7eec34e56b83?dpr=2&auto=compress,format&fit=crop&w=500&q=80&cs=tinysrgb&crop=&bg=";
 
 
 export default class HomePage extends React.Component {
-	handleRoute(next) {
-		return next();
-	}
+  handleRoute(next) {
+    return next();
+  }
 
-	getElements() {
-		return (
-			<RootElement>
-				<Content pageName='home' img={HOME_IMG}/>
-			</RootElement>
-		);
-	}
+  getElements() {
+    return (
+      <RootElement>
+        <Content pageName='home' img={HOME_IMG}/>
+      </RootElement>
+    );
+  }
 }
